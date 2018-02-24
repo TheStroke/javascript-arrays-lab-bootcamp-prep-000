cebd97b4d19cc67bd8167df278c80dbdbc577754
Aaron Gibson <aaron.gibson13@gmail.com>
2018-02-24 15:56:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 function destructivelyAppendKitten(entry) {
   //access an array, add an element to the end and return mutated new array
   
-  kitten.push(entry)
-  return array
+  kittens.push(entry)
+  return kittens
 }
