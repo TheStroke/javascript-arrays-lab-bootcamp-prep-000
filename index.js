12e2e86be7b04cc7a89c8e309ef96a13b9411147
Aaron Gibson <aaron.gibson13@gmail.com>
2018-02-24 16:05:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,3 +33,8 @@ function prependKitten(entry) {
   var cats = [entry, ...kittens]
   return cats
 }
+
+function removeLastKitten() {
+  var cats = kittens.slice(0, kittens.length - 1)
+  return cats
+}
